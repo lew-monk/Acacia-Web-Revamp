08e1e8993a2689cc7286593544f0955a2b214b8c
lew-monk <lewmonk@gmail.com>
2024-02-20 23:22:55 +0300

    Add Moment Section

--- src/lib/components/moments.svelte
+++ src/lib/components/moments.svelte
@@ -1,15 +1,49 @@
 <script>
 	import Cottages from '../images/F.avif';
+	import Banner from '../images/bannerImg.avif';
+	import Food from '../images/Rectangle (1).avif';
+	import Bed from '../images/bedroom.avif';
+	import Lounge from '../images/Rectangle 20 (1).avif';
+	import Lounger2 from '../images/Rectangle 20.avif';
+	import { MoveLeft, MoveRight } from 'lucide-svelte';
 	import { Button } from './ui/button';
 </script>
 
-<section class="py-24 bg-bgDark flex items-center">
+<section class="py-24 bg-[#DDE0C7] flex items-center">
 	<div class="grid gap-4">
 		<h2 class="text-xl text-primary relative oasis">Our Gallery</h2>
 
-		<h1 class="text-5xl leading-snug w-full text-textPrimary font-medium">
-			Moments at <span class="text-primary">Acacia</span>
-		</h1>
+		<div class="flex w-full items-center">
+			<h1 class="text-5xl leading-snug w-full text-textPrimary font-medium">
+				Moments at <span class="text-primary">Acacia</span>
+			</h1>
+			<div class="flex gap-4">
+				<Button
+					variant="outline"
+					class="shadow-none h-16 w-16 rounded-full border-2 border-primary"
+					size="icon"
+				>
+					<MoveLeft class="h-4 w-4" />
+				</Button>
+				<Button
+					variant="outline"
+					class="shadow-none h-16 w-16 rounded-full border-2 border-primary "
+					size="icon"
+				>
+					<MoveRight class="h-4 w-4" />
+				</Button>
+			</div>
+		</div>
+		<div class="grid grid-cols-3 gap-4 w-full pt-8">
+			<img src={Food} alt="" class="h-full object-cover rounded-[8px]" />
+			<img src={Banner} alt="" class="h-full rounded-[8px]" />
+			<div class="grid grid-cols-2 gap-4 grid-rows-2">
+				<img src={Lounge} alt="" class="w-full object-cover h-full rounded-[8px]" />
+				<img src={Cottages} alt="" class="w-full rounded-[8px]" />
+				<img src={Bed} alt="" class="h-full object-cover rounded-[8px]" />
+				<img src={Lounger2} alt="" class="h-full object-cover rounded-[8px]" />
+			</div>
+		</div>
 	</div>
 </section>
 
