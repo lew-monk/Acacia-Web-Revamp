a7177e63ff92a56d61068411891445157f58beaf
lew-monk <lewmonk@gmail.com>
2024-03-06 23:17:33 +0300

    Add A few animations

--- src/routes/security/+page.svelte
+++ src/routes/security/+page.svelte
@@ -3,11 +3,12 @@
 	import Footer from '$lib/components/footer.svelte';
 	import Cottages from '$lib/images/Gate.avif';
 	import Pool from '$lib/images/Pool-2.avif';
+	const images = [Cottages];
 </script>
 
 <section class="">
 	<div class=" carousel h-[80vh] flex justify-center items-center w-full relative">
-		<Carousel jump={true} />
+		<Carousel jump={true} {images} />
 		<div class="overlay h-full w-full bg-black absolute top-0 left-0 opacity-70"></div>
 		<div class="z-10 absolute text-white grid gap-8 text-center">
 			<h1 class="text-2xl font-bold">Safety and Security</h1>
