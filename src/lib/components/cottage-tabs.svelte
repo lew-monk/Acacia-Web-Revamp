967caf2fa61dd2e5e5579688d5f4ea079d567d95
lew-monk <lewmonk@gmail.com>
2024-03-07 17:39:27 +0300

    Add A few animations

--- src/lib/components/cottage-tabs.svelte
+++ src/lib/components/cottage-tabs.svelte
@@ -1,5 +1,10 @@
 <script>
 	import AccomodationImages from './accomodation-images.svelte';
+	import Bed from '$lib/images/bedroom.avif';
+	import Cottage from '$lib/images/F.avif';
+	import Towel from '$lib/images/0B2A9474.avif';
+
+	let images = [Bed, Cottage, Towel];
 </script>
 
-<AccomodationImages />
+<AccomodationImages {images} />
